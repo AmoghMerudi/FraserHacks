27015d1ad1cd1b6884a38c6f0dd7484875b28f98
AaronLiuDot <753677@pdsb.net>
2024-11-29 10:31:08 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
+function drawBackground() {
   background(220);
 
   push();
@@ -15,10 +15,11 @@ function draw() {
   education = text("Education", windowWidth/2, windowHeight/2)
   
   pop();
-
-  push();
-  fill(144, 238, 144);
-  rect(0, windowHeight - 150, windowWidth, windowHeight);
-  pop();
-  
 }
+
+function drawCharacter() {
+
+  box = new Player(200, 0, 20, windowWidth-20, windowHeight-1000);
+
+}
+
