fddad683d189da55031d02e5ef14e55d32b90219
AmoghMerudi <amoghmerudi@gmail.com>
2024-11-29 10:07:08 -0500

    "Updated Player constructor arguments in draw function"

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup() {
 function draw() {
   background(220);
   
-  box = new Player(200, 0, 20, windowWidth-20, windowHeight-1000);
+  box = new Player(healthCounter, damageCounter, 20, windowWidth-20, windowHeight-1000);
 
   push();
 
