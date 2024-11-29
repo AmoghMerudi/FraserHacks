f45c2b65926c3b1320ab9b52dd9b7df4e5d686d0
Krithik Ramkumar <krithikr914@gmail.com>
2024-11-29 09:47:35 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,12 +6,5 @@ function draw() {
   background(220);
   
   box = rect(200,200, 20, 20);
-
-  push();
-
-  textSize();
-  education = text("Education", windowWidth/2, windowHeight/2);
-
-  pop();
-
+  education = text("Education", windowWidth/2, windowHeight/2)
 }
